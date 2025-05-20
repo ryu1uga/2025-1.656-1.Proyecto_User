d2bc15f906fa6a1884915b5884c996610e20457b
LucasM <20221543@aloe.ulima.edu.pe>
2025-05-20 00:13:41 -0500

    Proyecto noche

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={ <LoginPage /> } />
         <Route path="/register" element={ <RegisterPage /> } />
+        <Route path="/home" element={ <RegisterPage /> } />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,16 +1,19 @@
+import { useNavigate } from "react-router-dom"
 import LoginForm from "../components/LoginForm"
 import "./LoginPage.css"
 
 const LoginPage = () => {
 
-    const handleLogin = (email: string, password: string, rememberMe: boolean) => {
+    const login = (email: string, password: string, rememberMe: boolean) => {
         console.log("Email:", email)
         console.log("Password:", password)
         console.log("Remember me:", rememberMe)
+        const navigate = useNavigate();
+        navigate("/home");
     }
 
     return <div className="container">
-        <LoginForm enviar={ handleLogin }></LoginForm>
+        <LoginForm enviar={ login }></LoginForm>
     </div>
 }
 
