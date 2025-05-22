8ae36c377ad8d39c5715e59b90c689baf24ff88f
unknown <20213654@aloe.ulima.edu.pe>
2025-05-22 18:28:00 -0500

    feat: añadida ruta /carrito

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import LoginPage from './pages/LoginPage.tsx'
 import RegisterPage from './pages/RegisterPage.tsx'
 import JuegoDetalle from './pages/JuegoDetalle';
+import PagoCarrito from './pages/CarroCompras/PagoCarrito';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
@@ -19,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/register" element={ <RegisterPage /> } />
         <Route path="/home" element={ <HomePage /> } />
         <Route path="/juego" element={<JuegoDetalle />} /> {/* Nueva ruta para los detalles del juego */}
+        <Route path="/carrito" element={<PagoCarrito />} /> {/* Nueva ruta */}
       </Routes>
     </BrowserRouter>
   </StrictMode>,
