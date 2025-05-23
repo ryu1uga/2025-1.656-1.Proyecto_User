df390eceec21f4ace09577553ee68494bdd3003c
unknown <20213654@aloe.ulima.edu.pe>
2025-05-22 23:05:21 -0500

    asd

---
+++ src/pages/ResenaPage/ResenaPage.tsx
@@ -0,0 +1,34 @@
+import './ResenaPage.css';
+
+const ResenaPage = () => {
+    return (
+        <div className="game-container">
+            <h1 className="game-title">Nombre del Juego</h1>
+            
+            <h3 className="section-title">Trailer</h3>
+            <div className="video-container">
+                <span>Video</span>
+            </div>
+            
+            <h3 className="section-title">Imagenes del Gameplay</h3>
+            <div className="gameplay-scroll">
+                <div className="image">Imagen 1</div>
+                <div className="image">Imagen 2</div>
+                <div className="image">Imagen 3</div>
+                <div className="image">Imagen 4</div>
+            </div>
+            
+            <div className="review-section">
+                <h3 className="section-title">Deja tu reseña</h3>
+                <textarea className="review-input" placeholder="Escribe tu comentario aquí"></textarea>
+                
+                <div className="action-buttons">
+                    <button className="btn btn-outline-secondary btn-action">Volver</button>
+                    <button className="btn btn-primary btn-action">Enviar</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ResenaPage;
